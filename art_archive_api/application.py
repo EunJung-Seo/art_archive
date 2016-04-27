0f5ace884328e48ee83c234281ddc29d9b7c5921
EunJung-Seo <soudg2@gmail.com>
2016-04-28 00:42:01 +0900

    ADD DB configuration

--- art_archive_api/application.py
+++ art_archive_api/application.py
@@ -1,6 +1,16 @@
+import os 
+
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URI")
+track_modifications = app.config.setdefault(
+    'SQLALCHEMY_TRACK_MODIFICATIONS',
+    True,
+)
+db = SQLAlchemy(app)
 
 @app.route('/')
 def hello():
