02ee8be69e5c9cf64afd0adeb51766a8b54df129
EunJung-Seo <soudg2@gmail.com>
2016-04-29 11:17:39 +0900

    ADD Error Handler (404, 500)

--- art_archive_api/views.py
+++ art_archive_api/views.py
@@ -67,3 +67,24 @@ def get_artist(id):
             "artist": json_data,
         }
     )
+
+# Error Response
+@app.errorhandler(404)
+def page_not_found(error):
+    endpoint = request.url_rule.endpoint
+    if endpoint == "get_artist":
+        response = jsonify(
+            {
+                "error": "Artist doesn't exist",
+            }
+        )
+    else:
+        response = jsonify({'error': 'Not Found'})
+    response.status_code = 404
+    return response
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    response = jsonify({'error': 'Internal Server Error'})
+    response.status_code = 500
+    return response
